3e557c3cef8600bd9950e10ae42f6a84387a3eb4
rame0 <r@me0.biz>
2023-03-10 14:08:10 +0600

    Changed config to my needs

--- src/config.ts
+++ src/config.ts
@@ -7,16 +7,23 @@ import { StrategyConfig } from './strategy.js';
 
 export const config: RobotConfig = {
   /** Используем реальный счет или песочницу */
-  useRealAccount: false,
+  useRealAccount: true,
   /** Уровень логирования */
-  logLevel: 'info',
+  //logLevel: 'info',
+  logLevel: 'warn',
   /** Используемые стратегии: */
   strategies: [
-    getStrategyConfig('BBG004731354'), // Роснефть
-    getStrategyConfig('BBG008F2T3T2'), // РУСАЛ
-    getStrategyConfig('BBG004S68829'), // Татнефть
-    getStrategyConfig('BBG000BN56Q9'), // Детский Мир
-    getStrategyConfig('BBG004730N88'), // Сбер
+//    getStrategyConfig('BBG004731354'), // Роснефть
+//    getStrategyConfig('BBG008F2T3T2'), // РУСАЛ
+//    getStrategyConfig('BBG004S68829'), // Татнефть
+//    getStrategyConfig('BBG000BN56Q9'), // Детский Мир
+//    getStrategyConfig('BBG004730N88'), // Сбер
+//     getStrategyConfig('TCS00A1039N1'), // TBRU
+    getStrategyConfig('BBG333333333'), // TMOS
+    getStrategyConfig('BBG000000001'), // TRUR
+    getStrategyConfigMVID('BBG004S68CP5'), // MVID
+    // getStrategyConfig('BBG00V7649K4'), // AKMB
+    // getStrategyConfig('TCS00A102YC6'), // Тинькофф SPAC
   ]
 };
 
@@ -27,22 +34,57 @@ function getStrategyConfig(figi: string): StrategyConfig {
     /** По сколько лотов покупаем/продаем */
     orderLots: 1,
     /** Комиссия брокера, % от суммы сделки */
-    brokerFee: 0.3,
+    brokerFee: 0.0,
     /** Интервал свечей */
-    interval: CandleInterval.CANDLE_INTERVAL_5_MIN,
+    interval: CandleInterval.CANDLE_INTERVAL_1_MIN,
     /** Конфиг сигнала по отклонению текущей цены */
     profit: {
       /** При каком % превышении цены продаем актив, чтобы зафиксировать прибыль */
-      takeProfit: 10,
+      takeProfit: 0.5,
       /** При каком % снижении цены продаем актив, чтобы не потерять еще больше */
-      stopLoss: 5,
+      stopLoss: 0.5,
     },
     /** Конфиг сигнала по скользящим средним */
     sma: {
       /** Кол-во точек для расчета быстрого тренда */
-      fastLength: 10,
+      fastLength: 50,
       /** Кол-во точек для расчета медленного тренда */
-      slowLength: 30,
+      slowLength: 200,
+    },
+    /** Конфиг сигнала по RSI */
+    rsi: {
+      /** Кол-во точек для расчета rsi */
+      period: 14,
+      /** Верхний уровень */
+      highLevel: 70,
+      /** Нижний уровень */
+      lowLevel: 30,
+    }
+  };
+}
+function getStrategyConfigMVID(figi: string): StrategyConfig {
+  return {
+    /** ID инструмента */
+    figi,
+    /** По сколько лотов покупаем/продаем */
+    orderLots: 1,
+    /** Комиссия брокера, % от суммы сделки */
+    brokerFee: 0.05,
+    /** Интервал свечей */
+    interval: CandleInterval.CANDLE_INTERVAL_1_MIN,
+    /** Конфиг сигнала по отклонению текущей цены */
+    profit: {
+      /** При каком % превышении цены продаем актив, чтобы зафиксировать прибыль */
+      takeProfit: 1,
+      /** При каком % снижении цены продаем актив, чтобы не потерять еще больше */
+      stopLoss: 1,
+    },
+    /** Конфиг сигнала по скользящим средним */
+    sma: {
+      /** Кол-во точек для расчета быстрого тренда */
+      fastLength: 50,
+      /** Кол-во точек для расчета медленного тренда */
+      slowLength: 200,
     },
     /** Конфиг сигнала по RSI */
     rsi: {
