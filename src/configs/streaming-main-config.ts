171de1a2c130d0f83d5a88d609c7373d3c18d92b
rame0 <r@me0.biz>
2023-10-24 08:28:27 +0600

    chore: конфиг тестовой стратегии

--- src/configs/streaming-main-config.ts
+++ src/configs/streaming-main-config.ts
@@ -10,50 +10,70 @@ export const config: RobotConfig = {
   useRealAccount: true,
   /** Уровень логирования */
   logLevel: 'info',
-  //logLevel: 'warn',
+  // logLevel: 'warn',
   /** Используемые стратегии: */
   strategies: [
-    { // TGLD
+//    { // TGLD
+//      /** ID инструмента */
+//      figi: 'TCS10A101X50',
+//      /** По сколько лотов покупаем/продаем */
+//      orderLots: 300,
+//      /** Комиссия брокера, % от суммы сделки */
+//      brokerFee: 0.0,
+//      /** Интервал свечей */
+//      candleInterval: CandleInterval.CANDLE_INTERVAL_1_MIN,
+//      subscriptionInterval: SubscriptionInterval.SUBSCRIPTION_INTERVAL_ONE_MINUTE,
+//      strategyType: StrategyTypes.stupid,
+//      profit: {
+//        /** При каком % превышении цены продаем актив, чтобы зафиксировать прибыль */
+//        takeProfit: 1.5,
+//        /** При каком % снижении цены продаем актив, чтобы не потерять еще больше */
+//        stopLoss: 1.5,
+//      },
+//
+//      keepOrdersAlive: {
+//        sell: 5,
+//        buy: 5
+//      }
+//    },
+    // { // KLSB
+    //   /** ID инструмента */
+    //   figi: 'BBG000DBD6F6',
+    //   /** По сколько лотов покупаем/продаем */
+    //   orderLots: 1,
+    //   /** Комиссия брокера, % от суммы сделки */
+    //   brokerFee: 0.05,
+    //   /** Интервал свечей */
+    //   candleInterval: CandleInterval.CANDLE_INTERVAL_1_MIN,
+    //   subscriptionInterval: SubscriptionInterval.SUBSCRIPTION_INTERVAL_ONE_MINUTE,
+    //   strategyType: StrategyTypes.profitRsiSMMA,
+    //   profit: { takeProfit: 0.6, stopLoss: 1 },
+    //   sma: { fastLength: 12, slowLength: 24 },
+    //   rsi: { period: 28, highLevel: 80, lowLevel: 20 },
+    //
+    //   keepOrdersAlive: {
+    //     sell: 0,
+    //     buy: 0,
+    //   }
+    // },
+    { // TCS Group
       /** ID инструмента */
-      figi: 'TCS10A101X50',
+      figi: 'BBG00QPYJ5H0',
       /** По сколько лотов покупаем/продаем */
-      orderLots: 100,
+      orderLots: 1,
       /** Комиссия брокера, % от суммы сделки */
       brokerFee: 0.0,
       /** Интервал свечей */
       candleInterval: CandleInterval.CANDLE_INTERVAL_1_MIN,
       subscriptionInterval: SubscriptionInterval.SUBSCRIPTION_INTERVAL_ONE_MINUTE,
-      strategyType: StrategyTypes.stupid,
-      profit: {
-        /** При каком % превышении цены продаем актив, чтобы зафиксировать прибыль */
-        takeProfit: 1.5,
-        /** При каком % снижении цены продаем актив, чтобы не потерять еще больше */
-        stopLoss: 1.5,
-      },
-
-      keepOrdersAlive: {
-        sell: 5,
-        buy: 5
-      }
-    },
-    { // KLSB
-      /** ID инструмента */
-      figi: 'BBG000DBD6F6',
-      /** По сколько лотов покупаем/продаем */
-      orderLots: 1,
-      /** Комиссия брокера, % от суммы сделки */
-      brokerFee: 0.05,
-      /** Интервал свечей */
-      candleInterval: CandleInterval.CANDLE_INTERVAL_1_MIN,
-      subscriptionInterval: SubscriptionInterval.SUBSCRIPTION_INTERVAL_ONE_MINUTE,
       strategyType: StrategyTypes.profitRsiSMMA,
-      profit: { takeProfit: 0.2, stopLoss: 0.1 },
+      profit: { takeProfit: 0.5, stopLoss: 0.4 },
       sma: { fastLength: 12, slowLength: 24 },
       rsi: { period: 28, highLevel: 80, lowLevel: 20 },
 
       keepOrdersAlive: {
-        sell: 0,
-        buy: 0,
+        sell: 2,
+        buy: 2
       }
     }
   ]
