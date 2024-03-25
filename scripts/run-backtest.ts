1ceb139ab6dfa571ce31c14979f023160636a2ff
rame0 <r@me0.biz>
2024-03-25 18:49:57 +0300

    fix: переписал backtest

--- scripts/run-backtest.ts
+++ scripts/run-backtest.ts
@@ -9,24 +9,68 @@
 import MockDate from 'mockdate';
 import { Helpers } from 'tinkoff-invest-api';
 import { Robot } from '../src/robot.js';
-import { config } from '../src/configs/config-klsb.js';
 import { OperationState, OperationType } from 'tinkoff-invest-api/dist/generated/operations.js';
 import { backtestApi as api } from './init-api.js';
 import { StrategyConfig } from "../src/baseStrategy";
 import { CandleInterval } from "tinkoff-invest-api/dist/generated/marketdata.js";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import { logger } from "yandex-cloud-deploy-fn/dist/helpers/logger";
 
-let start_n = 0;
-if (process.argv[ 2 ]) {
-  start_n = Number(process.argv[ 2 ]);
+interface Arguments extends yargs.Arguments {
+  start_n: number;
+  interval: number;
+  config: string;
+  streaming: boolean;
+  start: string;
+  end: string;
 }
 
-let strategyType = 0;
-if (process.argv[ 3 ]) {
-  strategyType = Number(process.argv[ 3 ]);
-}
+const args: Arguments = yargs(hideBin(process.argv))
+  .option('start_n', {
+    global: true,
+    alias: 's',
+    type: 'number',
+    description: 'Номер стратегии из конфига, которую будем тестировать',
+    default: 0,
+  })
+  .option('interval', {
+    global: true,
+    alias: 'i',
+    type: 'number',
+    description: 'Интервал свечей для текущего теста (1 - 1мин., 2 - 5мин., 3 - 15мин., 4 - 1час). Он заменит ' +
+      'интервал указанный в файле конфигурации',
+    default: 1,
+  })
+  .option('streaming', {
+    global: true,
+    type: 'boolean',
+    description: 'Запуск стримингового робота',
+    default: false,
+  })
+  .option('config', {
+    global: true,
+    alias: 'c',
+    type: 'string',
+    description: 'Имя файла конфигурации из папки src/configs',
+  })
+  .option('start', {
+    global: true,
+    alias: 'f',
+    type: 'string',
+    description: 'Начало периода бэктеста (формат: ГГГГ/ММ/ДД)',
+  })
+  .option('end', {
+    global: true,
+    alias: 'e',
+    type: 'string',
+    description: 'Конец периода бэктеста (формат: ГГГГ/ММ/ДД)',
+  })
+  .help()
+  .parse() as Arguments;
 
 let interval: CandleInterval = CandleInterval.UNRECOGNIZED;
-switch (Number(process.argv[ 4 ])) {
+switch (args.interval) {
   case 1:
     interval = CandleInterval.CANDLE_INTERVAL_1_MIN;
     break;
@@ -39,48 +83,53 @@ switch (Number(process.argv[ 4 ])) {
   case 4:
     interval = CandleInterval.CANDLE_INTERVAL_HOUR;
     break;
+  default:
+    console.log('interval not set');
+    process.exit();
 }
 
-if (interval == CandleInterval.UNRECOGNIZED) {
-  console.log('interval not set');
-  process.exit();
-} else {
-  console.log(`interval: ${interval.toString()}`);
-}
+console.log(`interval: ${interval.toString()}`);
 
 // Диапазон дат для бэктеста
-const from = new Date('2023-06-24T00:00:00+03:00');
-const to = new Date('2023-07-24T23:59:00+03:00');
+const from = new Date(`${args.start}T00:00:00+03:00`);
+const to = new Date(`${args.end}T23:59:00+03:00`);
 
 // Для бэктеста оставляем тестируем по одной стратегии за раз
-const strategies = config.strategies;
+const module = await import(`../src/configs/${args.config}.js`);
+const config = module.config;
 
-config.strategies = strategies.slice(start_n, start_n + 1);
-console.log(config.strategies);
-if (config.strategies.length < 1)
+if (!config.strategies || config.strategies.length < 1) {
+  console.log('No strategies in config');
   process.exit();
+}
+if (config.strategies[ args.start_n ] === undefined) {
+  console.log('No strategy with this number');
+  process.exit();
+}
 
-const strategyConfig = config.strategies[ 0 ];
+const strategyConfig = config.strategies[ args.start_n ];
 strategyConfig.candleInterval = interval;
-
-console.log(`strategyType: ${strategyType}`);
+config.strategies = [ strategyConfig ];
 
 void main(strategyConfig);
 
 async function main(strategyConfig: StrategyConfig) {
   await configureBroker({ from, to, candleInterval: strategyConfig.candleInterval });
-  const finalConfig = { ...config, logLevel: 'none' };
-  const robot = new Robot(api, finalConfig);
+  const robot = new Robot(api, { ...config, logLevel: 'none' });
 
-  while (await tick()) {
-    await robot.run();
+  try {
+    while (await tick()) {
+      await robot.run(true);
+    }
+  } catch (e) {
+    robot.logger.error(e.details || e.message);
   }
 
   // await showOperations(strategyConfig);
   await countOperations(strategyConfig);
   await showExpectedYield();
   robot.strategies.map(s => console.log(`Profit from strategy run: ${s.totalProfit}`));
-  // buildCharts(robot);
+  buildCharts(robot);
 }
 
 async function showExpectedYield() {
@@ -151,18 +200,20 @@ async function tick() {
   }
 }
 
-//
-// function buildCharts(robot: Robot) {
-//   const strategy = robot.strategies[0];
-//   const charts = strategy.smaSignal?.charts;
-//   if (!charts) return;
-//
-//   const series = Object.keys(charts).map(name => ({name, data: charts[name].map(val => [(val[0].getTime()), val[1]])}));
-//   // const series = Object.keys(charts).map(name => ({name, data: charts[name]}));
-//   const seriesContent = JSON.stringify(series);
-//   const tpl = fs.readFileSync('chart/index.tpl.js', 'utf8');
-//   const newContent = tpl
-//     .replace('%ticker%', strategy.instrument.info?.ticker || strategy.config.figi)
-//     .replace('series: []', `series: ${seriesContent}`);
-//   fs.writeFileSync('chart/index.js', newContent);
-// }
+function buildCharts(robot: Robot) {
+  // const strategy = robot.strategies[ 0 ];
+  // const charts = strategy.smaSignal?.charts;
+  // if (!charts) return;
+  //
+  // const series = Object.keys(charts).map(name => ({
+  //   name,
+  //   data: charts[ name ].map(val => [(val[ 0 ].getTime()), val[ 1 ]])
+  // }));
+  // // const series = Object.keys(charts).map(name => ({name, data: charts[name]}));
+  // const seriesContent = JSON.stringify(series);
+  // const tpl = fs.readFileSync('chart/index.tpl.js', 'utf8');
+  // const newContent = tpl
+  //   .replace('%ticker%', strategy.instrument.info?.ticker || strategy.config.figi)
+  //   .replace('series: []', `series: ${seriesContent}`);
+  // fs.writeFileSync('chart/index.js', newContent);
+}
